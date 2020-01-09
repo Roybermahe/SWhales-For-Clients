1b716e4d6e7b877ef4f40f2d99345f2d39c3d1db
Royber <roybermanjarrez@gmail.com>
2020-01-09 17:38:40 -0500

    Actualizado app.module.ts
    Se actualiza por eliminación del componente genérico item

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,6 @@ import { ItemsComponent } from "./item/items.component";
 import { ItemDetailComponent } from "./item/item-detail.component";
 
 const routes: Routes = [
-    { path: "", redirectTo: "/items", pathMatch: "full" },
-    { path: "items", component: ItemsComponent },
-    { path: "item/:id", component: ItemDetailComponent }
 ];
 
 @NgModule({
